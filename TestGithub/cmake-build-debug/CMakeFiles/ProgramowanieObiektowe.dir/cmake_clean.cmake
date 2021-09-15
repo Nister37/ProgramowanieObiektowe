7382e0684d298ec6edb7db1ade5eb25bef885bf3
Nister37 <pawelqbc@gmail.com>
2021-09-15 11:08:35 +0200

    first commit

---
+++ TestGithub/cmake-build-debug/CMakeFiles/ProgramowanieObiektowe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgramowanieObiektowe.dir/main.cpp.obj"
+  "ProgramowanieObiektowe.exe"
+  "ProgramowanieObiektowe.exe.manifest"
+  "ProgramowanieObiektowe.pdb"
+  "libProgramowanieObiektowe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProgramowanieObiektowe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
